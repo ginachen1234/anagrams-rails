afbd3e2506f87ed69a608d9216d8e0682c135e5f
gina chen <ginachen588@gmail.com>
2019-05-20 09:08:11 +0100

    query for amagrams

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
     uri = URI('https://gist.githubusercontent.com/Kalagan/3b26be21cbf65b62cf05ab549433314e/raw')
       data = Net::HTTP.get(uri)
       anagrams = data.split(/\n/)
+    # @user = User.find(params[:id])
       @query = params[:query]
       @results = anagrams.select { |word| @query.split('').sort.join == word.split('').sort.join }
     # @vari = User.my_method
